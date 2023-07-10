e18d6bbf2b0106613f2915f9595e0c4fa2fb9d51
timowin <timowinkler44@googlemail.com>
2023-07-10 14:14:39 +0200

    refactor rooftypeselector to use radio instead of select

--- app/requests/new/components02/RoofTypeSelector.tsx
+++ app/requests/new/components02/RoofTypeSelector.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React, { ReactNode, useState } from 'react'
 import RoofType from './RoofType'
 
 import sattelDach from 'public/satteldach.png'
 import pultDach from 'public/pultdach.png'
 import flachDach from 'public/flachdach.png'
 import walmDach from 'public/walmdach.png'
-import { Select, Option } from '@/app/materialComponents'
+import { Radio } from '@/app/materialComponents'
 
-type Props = {}
+const RoofTypeSelector = () => {
 
-const RoofTypeSelector = (props: Props) => {
+
   return (
-    <Select>
-      <Option><RoofType isSelected={false} image={walmDach} name='Walmdach' description='Ein Dach mit vier geneigten Seiten, bei dem die beiden Seitenwände ebenfalls abgeschrägt sind.'/></Option>
-      <Option><RoofType isSelected={false} image={flachDach} name='Flachdach' description='Ein horizontales Dach (mit Attika) ohne sichtbare Neigung, das ein modernes Erscheinungsbild verleiht.'/></Option>
-      <Option><RoofType isSelected={false} image={pultDach} name='Pultdach' description='Ein asymmetrisches Dach mit einer geneigten Fläche, die an eine Wand angrenzt und eine einfache Konstruktion bietet.'/></Option>
-      <Option><RoofType isSelected={false} image={sattelDach} name='Satteldach' description='Ein traditionelles Dach mit zwei geneigten Seiten, die in der Mitte zusammentreffen.'/></Option>
-    </Select>
+    <div className='grid grid-cols-2 gap-8'>
+      <Radio name="roofType" id="walm" label={<RoofType image={walmDach} name='Walmdach' description='Ein Dach mit vier geneigten Seiten, bei dem die beiden Seitenwände ebenfalls abgeschrägt sind.'/>} />
+      <Radio name="roofType" id="flat" label={<RoofType image={flachDach} name='Flachdach' description='Ein horizontales Dach (mit Attika) ohne sichtbare Neigung, das ein modernes Erscheinungsbild verleiht.'/>} />
+      <Radio name="roofType" id="mono" label={<RoofType image={pultDach} name='Pultdach' description='Ein asymmetrisches Dach mit einer geneigten Fläche, die an eine Wand angrenzt und eine einfache Konstruktion bietet.'/>} />
+      <Radio name="roofType" id="gable" label={<RoofType image={sattelDach} name='Satteldach' description='Ein traditionelles Dach mit zwei geneigten Seiten, die in der Mitte zusammentreffen.'/>} />
+    </div>
   )
 }
 
